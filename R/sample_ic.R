b51a85b4127a37cfcbf0451cbbc102e0c68260f2
princyparsana <princyp89@gmail.com>
2020-01-30 14:21:43 -0500

    add option for uniform support input

--- R/sample_ic.R
+++ R/sample_ic.R
@@ -1,5 +1,5 @@
 ## construct a sparse inverse covariance matrix
-sample_ic <- function(p = 10, type = "scale-free"){
+sample_ic <- function(p = 10, type = "scale-free" ,min_support = c(-0.4, 0.1), max_support = c(0.1, 0.4)){
   if(type == "scale-free"){
     pg = igraph::as.undirected(igraph::sample_pa(p, directed = T))
   }else{
@@ -8,7 +8,7 @@ sample_ic <- function(p = 10, type = "scale-free"){
   n_edges = igraph::ecount(pg)
   g <- igraph::as_adj(pg)
   g <- Matrix::triu(g)
-  e_vals = runif(n_edges, min = c(-0.9, 0.5), max = c(-0.5, 0.9))
+  e_vals = runif(n_edges, min = c(-0.4, 0.1), max = c(0.1, 0.4))
   g[g!=0] <- e_vals
   g <- convert_psd(g)
   if(!all(eigen(g)$values >=0)){
