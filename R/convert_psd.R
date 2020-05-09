fef8f81dbdc40641371fed1331325b9d7efeea38
princyparsana <princyp89@gmail.com>
2020-05-09 15:53:08 -0400

    add two ways of converting network to PSD matrix

--- R/convert_psd.R
+++ R/convert_psd.R
@@ -1,22 +1,33 @@
 library(Matrix)
 library(igraph)
 
-convert_psd <- function(thisgraph){
+convert_psd <- function(thisgraph, psd_type = "jgl", v  = 0.3, u = 0.1){
   thisgraph <- as.matrix(thisgraph)
-  rowsum_graph = rowSums(abs(as.matrix(thisgraph))) * 1.5
-  for(i in 1:length(rowsum_graph)){
-    thisgraph[i,] <- thisgraph[i,]/rowsum_graph[i]
-  }
-  #   if(sum(abs(x))!=0){
-  #     scaled_val = x/(1.5*sum(abs(x)))
-  #   }else{
-  #     x
-  #   }
-  # })
-  thisgraph[is.nan(thisgraph)]<-0
+  if(psd_type == "jgl"){
+    rowsum_graph = rowSums(abs(as.matrix(thisgraph))) * 1.5
+    for(i in 1:length(rowsum_graph)){
+      thisgraph[i,] <- thisgraph[i,]/rowsum_graph[i]
+    }
+    #   if(sum(abs(x))!=0){
+    #     scaled_val = x/(1.5*sum(abs(x)))
+    #   }else{
+    #     x
+    #   }
+    # })
+    thisgraph[is.nan(thisgraph)]<-0
 
-  diag(thisgraph) <- 1.00
-  thisgraph = (thisgraph + t(thisgraph))/2
+    diag(thisgraph) <- 1.00
+    thisgraph = (thisgraph + t(thisgraph))/2
+  }
+
+  if(psd_type == "huge"){
+    ## adapted from huge package, allows to control the magnitude of PSD precision matrix
+    diag(thisgraph) <- 0
+    theta = thisgraph*v
+    diag(theta) = abs(min(eigen(theta)$values)) + 0.1 + u
+    sigma = cov2cor(solve(theta))
+    thisgraph = solve(sigma)
+  }
 
   thisgraph
 }
--- R/sample_ic.R
+++ R/sample_ic.R
@@ -1,5 +1,5 @@
 ## construct a sparse inverse covariance matrix
-sample_ic <- function(p = 10, m=1, type = "scale-free" ,min_support = c(-0.4, 0.1), max_support = c(-0.1, 0.4), seed = 1){
+sample_ic <- function(p = 10, m=1, psd_type = "jgl", type = "scale-free" ,min_support = c(-0.4, 0.1), max_support = c(-0.1, 0.4), seed = 1){
   set.seed(seed)
   if(type == "scale-free"){
     pg = igraph::sample_pa(p, power = 2.5, m = m, directed = F)
@@ -11,7 +11,7 @@ sample_ic <- function(p = 10, m=1, type = "scale-free" ,min_support = c(-0.4, 0.
   g <- Matrix::triu(g)
   e_vals = runif(n_edges, min = min_support, max = max_support)
   g[g!=0] <- e_vals
-  g <- convert_psd(g)
+  g <- convert_psd(g, psd_type = psd_type)
   if(!all(eigen(g)$values >=0)){
     cat("PSD check unsuccessful - negative eigenvalues")
   }else{
