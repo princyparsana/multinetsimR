c05ce4d1e602d1b3120a4cbdcf7be1cda9334ff1
princyparsana <princyp89@gmail.com>
2020-06-09 13:43:38 -0400

    add the cov conversion as described in jgl

--- R/convert_psd.R
+++ R/convert_psd.R
@@ -18,11 +18,18 @@ convert_psd <- function(thisgraph, psd_type = "jgl", v  = 0.3, u = 0.1){
 
     diag(thisgraph) <- 1.00
     thisgraph = (thisgraph + t(thisgraph))/2
+    inv_thisgraph <- solve(thisgraph)
+    denominator_cov <- tcrossprod(diag(inv_thisgraph))
+    d_mat <- matrix(0.6, nrow = nrow(inv_thisgraph), ncol = ncol(inv_thisgraph))
+    diag(d_mat) <- 1
+    cov_mat <- d_mat/sqrt(denominator_cov)
+    thisgrpah = solve(cov_mat)
   }
 
   if(psd_type == "huge"){
     ## adapted from huge package, allows to control the magnitude of PSD precision matrix
     thisgraph  = thisgraph + t(thisgraph)
+    thisgraph[thisgraph!=0] <- 1
     diag(thisgraph) <- 0
     theta = thisgraph*v
     diag(theta) = abs(min(eigen(theta)$values)) + 0.1 + u
