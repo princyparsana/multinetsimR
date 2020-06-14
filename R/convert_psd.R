af5b6e06b82a8721c62fb296e1866f80eea4e58a
princyparsana <princyp89@gmail.com>
2020-06-14 01:19:15 -0400

    bug fixes

--- R/convert_psd.R
+++ R/convert_psd.R
@@ -4,6 +4,7 @@ library(igraph)
 convert_psd <- function(thisgraph, psd_type = "jgl", v  = 0.3, u = 0.1){
   thisgraph <- as.matrix(thisgraph)
   if(psd_type == "jgl"){
+    thisgraph <- thisgraph + t(thisgraph)
     rowsum_graph = rowSums(abs(as.matrix(thisgraph))) * 1.5
     for(i in 1:length(rowsum_graph)){
       thisgraph[i,] <- thisgraph[i,]/rowsum_graph[i]
@@ -22,15 +23,15 @@ convert_psd <- function(thisgraph, psd_type = "jgl", v  = 0.3, u = 0.1){
     denominator_cov <- tcrossprod(diag(inv_thisgraph))
     d_mat <- matrix(0.6, nrow = nrow(inv_thisgraph), ncol = ncol(inv_thisgraph))
     diag(d_mat) <- 1
-    cov_mat <- d_mat/sqrt(denominator_cov)
+    cov_mat <- (d_mat*inv_thisgraph)/sqrt(denominator_cov)
     thisgrpah = solve(cov_mat)
   }
 
   if(psd_type == "huge"){
     ## adapted from huge package, allows to control the magnitude of PSD precision matrix
     thisgraph  = thisgraph + t(thisgraph)
-    thisgraph[thisgraph!=0] <- 1
     diag(thisgraph) <- 0
+    thisgraph[thisgraph!=0] <- 1
     theta = thisgraph*v
     diag(theta) = abs(min(eigen(theta)$values)) + 0.1 + u
     sigma = cov2cor(solve(theta))
